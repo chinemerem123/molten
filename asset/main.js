40391af5dd490fe53f553bb369a0b840787b4b9e
Nnaji Christian <54823349+chinemerem123@users.noreply.github.com>
2023-02-07 02:08:18 -0800

    commit

--- asset/main.js
+++ asset/main.js
@@ -1,2 +1,14 @@
 
   AOS.init();
+
+  let btn=document.querySelector(".btn-start");
+
+ window.addEventListener('scroll', ()=>{
+  btn.style.backgroundColor="#3B23E6";
+  btn.style.padding="8px";
+  btn.style.border="none";
+  btn.style.boxShadow="10px 10px 30px rgba(0,0,0,0.2)";
+  document.querySelector(".nav-header").style.backgroundColor="white";
+  })
+
+  
